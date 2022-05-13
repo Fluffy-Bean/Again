90cf5a6112b73d94b4b3ace664826911dcd91aac
Michal <86935712+Fluffy-Bean@users.noreply.github.com>
2022-05-13 16:23:52 +0100

    Yeet

---
+++ levelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoader : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ timerText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class timerText : MonoBehaviour
+{
+
+    TextMeshProUGUI textBox;
+
+    public float time = 0;
+    protected float Timer;
+    public int delayAmount = 1;
+
+    void Start()
+    {
+        textBox = GetComponent<TextMeshProUGUI>();
+        textBox.SetText("0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "game")
+        {
+            Timer += Time.deltaTime;
+
+            if (Timer >= delayAmount)
+            {
+                Timer = 0f;
+                time++;
+                textBox.SetText("{0}", time);
+            }
+        }
+    }
+}
