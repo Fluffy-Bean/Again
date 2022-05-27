851c3aeffa8410e9e9b033ae82fd47e01888654b
Fluffy-Bean <michal-gdula@protonmail.com>
2022-05-27 04:58:03 +0100

    Merged common scripts

---
+++ menuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+  // Scene changer
+  public void NextLevel()
+  {
+    SceneManager.LoadScene("game");
+    Time.timeScale = 1f;
+  }
+  public void MainMenu()
+  {
+    SceneManager.LoadScene("menu");
+  }
+  public void Settings()
+  {
+    SceneManager.LoadScene("settings");
+  }
+
+  // Buttons
+  public void ExitGame()
+  {
+    Application.Quit();
+  }
+  public void ResetScore()
+  {
+    PlayerPrefs.SetFloat("lowestTime",999999f);
+  }
+
+  // Sliders
+  public AudioMixer audioMixer;
+  public void SetVolume(float volume)
+  {
+    audioMixer.SetFloat("Volume", volume);
+  }
+}
